5c4802b85791599b3bcc857dc11f5f4939e5e7da
ashish-ungerboeck <ashish.gupta@ungerboeck.com>
2024-07-20 18:45:54 +0530

    initial commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { createUser, findUserByEmail } = require('../models/userModel');
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await createUser(username, email, hashedPassword);
+        res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating user', error });
+    }
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid Credentials' });
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: 'User logged in successfully', token });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error });
+    }
+};
+
+module.exports = {
+    register,
+    login
+};
