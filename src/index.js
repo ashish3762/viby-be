b86b94031984078ff0fffe0ea0ef08aa2e21f582
ashish-ungerboeck <ashish.gupta@ungerboeck.com>
2024-07-21 03:51:14 +0530

    Fixed CORS issue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://viby-fe.vercel.app",
+  "https://viby-fe-ashish3762s-projects.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
