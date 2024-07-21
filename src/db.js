5e51e5fe553affad83f395ad31e1e2fd57239940
ashish-ungerboeck <ashish.gupta@ungerboeck.com>
2024-07-21 20:15:53 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('Connecting to database:', process.env.SUPABASE_DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to database');
+  release();
 });
 
 module.exports = pool;
