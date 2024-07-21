2a2552960aa2a75f4d0b56cdd0336e595b02153b
ashish-ungerboeck <ashish.gupta@ungerboeck.com>
2024-07-21 19:12:14 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'viby_db',
-  password: 'admin',
-  port: 5432,
+  connectionString: process.env.SUPABASE_DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
